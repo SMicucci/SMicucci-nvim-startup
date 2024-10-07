ef24ea78c627fd169fe7ba5cda9926dc5af2a94d
Simone Micucci <s.micucci00@gmail.com>
2024-10-07 08:45:29 +0200

    SM: changed open page, small changes

--- lua/plugin/alpha.lua
+++ lua/plugin/alpha.lua
@@ -3,51 +3,49 @@ if vim.g.plugs["alpha-nvim"] ~= nil then
 	local dashboard = require('alpha.themes.dashboard')
 
 	--	#custom setup
-	dashboard.section.header.val = {
-		[[          #                              #]],
-		[[        #####                            # #]],
-		[[      #########                          #   #]],
-		[[    #############                        #     #]],
-		[[  # ###############                      #       #]],
-		[[#   #################                    #         #                                 #####]],
-		[[#     ################                   #         #                               #######]],
-		[[#       ################                 #         #                               #######]],
-		[[#         ################               #         #                                 #####]],
-		[[#           ################             #         #   ##########     ###########       ##     ###########]],
-		[[#         #  ################            #         #     #     #        #######    ###           #     #    #####          #####]],
-		[[#         #    ################          #         #     #     #       #######     #  ###        #     #  #########      #########]],
-		[[#         #      ################        #         #     #     #      #######      #     #       #     # ############  #############]],
-		[[#         #        ################      #         #     #     #     #######       #     #       #     # ####  #######  #####  ######]],
-		[[#         #          ################    #         #     #     #    #######        #     #       #     # ###     ######  ##      #####]],
-		[[#         #            ################  #         #     #     #   #######         #     #       #     # #         #####          #####]],
-		[[#         #              ################          #     #     #  #######          #    ##       #     #            ####           ####]],
-		[[#         #                ################        #     #     # #######           #    ##       #     #            ####           ####]],
-		[[#         #                  ################      #     #      #######            #    ##       #     #            ####           ####]],
-		[[#         #                    ################    #     #     #######             #    ##       #     #            ####           ####]],
-		[[#         #                      ################  #     #    #######              #   ###       #     #            ####           ####]],
-		[[  #       #                        ################      #   #######               #   ###       #     #            ####           ####]],
-		[[    #     #                          #############       #  #######                #   ###       #     #            ####           ####]],
-		[[      #   #                            #########         # #######                 #   ###       #     #            ####           ####]],
-		[[        # #                              #####           ########                  #  ####       #     #            ####           ####]],
-		[[          #                                #             #######                 ###########   ###########        ########       ########]],
-	}
+	
+local logo = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
+
+
+	dashboard.section.header.val = logo
+
 	dashboard.section.buttons.val = {
-		dashboard.button("\\ff",	"󰱼   " .. "Fuzzy Finder",			"<cmd> Telescope find_files<CR>"),
-		dashboard.button("\\fr",	"󱎸   " .. "Text Finder",			"<cmd> Telescope live_grep<CR>"),
-		dashboard.button("\\bb",	"   " .. "File Buffer",			"<cmd> Telescope buffers<CR>"),
-		dashboard.button("\\gf",	"   " .. "Git Fugitive",			"<cmd> Git<CR><C-w>o"),
-		dashboard.button("\\d",		"   " .. "File Explorer",			"<cmd> Explore<CR>"),
-		dashboard.button("\\cc",	"   " .. "ChatGPT prompt",			"<cmd> ChatGPT<CR>"),
-		dashboard.button("\\tt",	"󱞟   " .. "Toggle Transparence",	"<cmd> TransparentToggle<CR>"),
+		dashboard.button("\\ff",	"󰮗  " .. "Fuzzy Finder",			"<cmd> Telescope find_files<CR>"),
+		dashboard.button("\\fr",	"󱎸  " .. "Text Finder",			"<cmd> Telescope live_grep<CR>"),
+		dashboard.button("\\bb",	"  " .. "File Buffer",			"<cmd> Telescope buffers<CR>"),
+		dashboard.button("\\gf",	"  " .. "Git Fugitive",			"<cmd> Git<CR><C-w>o"),
+		dashboard.button("\\d",		"  " .. "File Explorer",			"<cmd> Explore<CR>"),
+		dashboard.button("\\cc",	"  " .. "ChatGPT prompt",			"<cmd> ChatGPT<CR>"),
+		dashboard.button("\\tt",	"󱞟  " .. "Toggle Transparence",	"<cmd> TransparentToggle<CR>"),
 	}
 	for _, button in ipairs(dashboard.section.buttons.val) do
-		button.opts.hl = "Keyword"
+		button.opts.hl = "String"
 		button.opts.hl_shortcut = "Number"
 	end
+
+	dashboard.section.footer.val = {
+		[[]],
+		[[󱐋   Blazingly fast   󱐋]],
+		[[ Knoledge is power  ]],
+		[[      SMicucci]],
+	}
+
 	dashboard.section.header.opts.hl = "Function"
-	dashboard.section.buttons.opts.hl = "String"
-	dashboard.section.footer.opts.hl = "Statement"
-	dashboard.opts.layout[1].val = 4
+	dashboard.section.buttons.opts.hl = "Tabline"
+	dashboard.section.footer.opts.hl = "Keyword"
+	dashboard.opts.layout[1].val = 1
+	dashboard.opts.layout[3].val = 2
+	dashboard.section.terminal.width = 100
+	dashboard.section.terminal.height = 9
 
 	--	#default setup
 	alpha.setup(dashboard.config)
