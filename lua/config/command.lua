43d9185f1f24477cf9e486c47247f44cae4d338f
Simone Micucci <s.micucci00@gmail.com>
2025-01-09 01:56:37 +0100

    SM: minor fix, missing last point for Find command

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -14,39 +14,58 @@ M.au('termopen', {
 })
 
 M.cmd('FoldToggle',function ()
-  if vim.fn.foldclosed('.') ~= -1 then
-    vim.cmd('normal! zo')
-  else
-    vim.cmd('normal! zc')
-    if vim.fn.foldclosed('.') == vim.fn.line('.') then
-      vim.cmd('normal! j')
-    end
-  end
-end,{
-    desc = 'toggle fold element'
-  }
-)
-
--- wrapper for :grep with 'rg' or cascade to 'grep' or 'vimgrep'
--- TODO: end this command
-M.cmd('Find', function (opts)
-  -- grep can't search nothing
-  if #opts.fargs == 0 then
-    vim.notify('Find require at least one word to grep into work directory',vim.log.levels.WARN)
-    return
-  end
-  -- debug for now, ready to be thrown
-  vim.notify(vim.inspect(opts), vim.log.levels.DEBUG)
-  local regex
-  for i, val in ipairs(opts.fargs) do
-    if i == 1 then
-      regex = val
+  ---@diagnostic disable-next-line: param-type-mismatch
+  if vim.fn.foldlevel('.') > 0 then
+    ---@diagnostic disable-next-line: param-type-mismatch
+    if vim.fn.foldclosed('.') ~= -1 then
+      vim.cmd('normal! zo')
     else
-      regex = regex .. '\\ ' .. val
+      pcall(vim.cmd('normal! zc'))
+      ---@diagnostic disable-next-line: param-type-mismatch
+      if vim.fn.foldclosed('.') == vim.fn.line('.') then
+        vim.cmd('normal! j')
+      end
     end
   end
-  print(regex)
 end, {
+    desc = 'toggle fold element'
+  })
+
+-- TODO: end this command
+M.cmd('Find',
+  function (opts)
+    -- help message or something
+    if #opts.fargs == 0 then
+      vim.notify('󰀦 Find require at least one argument\n',vim.log.levels.WARN)
+      vim.notify('  input are used as regular expression directly',vim.log.levels.INFO)
+      return
+    end
+    -- debug for now, ready to be thrown
+    vim.notify(vim.inspect(opts.args), vim.log.levels.DEBUG)
+    vim.notify(vim.inspect(opts.fargs), vim.log.levels.DEBUG)
+    local regex
+    for i, val in ipairs(opts.fargs) do
+      if i == 1 then
+        regex = val
+      else
+        regex = regex .. ' ' .. val
+      end
+    end
+    print('result => ' .. vim.inspect(regex))
+    local rg = vim.fn.executable('rg')
+    local grep = vim.fn.executable('grep')
+    if rg then
+      -- run :grep with ripgrep
+    elseif grep then
+      vim.notify('󰀦 ripgrep not installed (recommended)',vim.log.levels.WARN)
+      -- run :grep with grep
+    else
+      vim.notify('󰀦 grep not installed too',vim.log.levels.WARN)
+      -- run :vimgrep
+    end
+    -- print(vim.inspect(rg_exist))
+  end,
+  {
     nargs = '*',
     desc = 'wrapper for :grep with \'rg\' or cascade to \'grep\' or \'vimgrep\''
   }
