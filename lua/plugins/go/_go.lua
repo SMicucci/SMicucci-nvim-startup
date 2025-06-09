1a299669f932bf466bcda6346c842cff873db7fa
SMicucci <s.micucci00@gmail.com>
2025-06-09 15:27:27 +0200

    fix(go): templ doesn't conflict anymore

--- lua/plugins/go/_go.lua
+++ lua/plugins/go/_go.lua
@@ -1,38 +1,37 @@
 return {
-  'https://github.com/ray-x/go.nvim',
-  dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod', 'gohtmltmpl', 'templ' },
-  build = ':lua require("go.install").update_all_sync()',
-  config = function()
-    require("go").setup()
+	"https://github.com/ray-x/go.nvim",
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	event = { "CmdlineEnter" },
+	ft = { "go", "gomod", "gohtmltmpl", "templ" },
+	build = ':lua require("go.install").update_all_sync()',
+	config = function()
+		require("go").setup()
 
-    local k = require 'config.keymap'
-    local auto = require 'config.command'
-    local mauto = require 'mason-automation'
+		local k = require("config.keymap")
+		local auto = require("config.command")
+		local mauto = require("mason-automation")
 
-    k.nmap('<space>go', '<cmd>GoPkgOutline<cr>','toggle project package and API', {silent = true})
+		k.nmap("<space>go", "<cmd>GoPkgOutline<cr>", "toggle project package and API", { silent = true })
 
-    local go_group = auto.aug('GoFormat', {})
+		local go_group = auto.aug("GoFormat", {})
 
-    auto.au('BufWritePre', {
-      pattern = '*.go',
-      group = go_group,
-      callback = function ()
-        require'go.format'.goimports()
-      end,
-      desc = 'Go auto import and format before write'
-    })
+		auto.au("BufWritePre", {
+			pattern = "*.go",
+			group = go_group,
+			callback = function()
+				require("go.format").goimports()
+			end,
+			desc = "Go auto import and format before write",
+		})
 
-    mauto.install('templ')
----@diagnostic disable-next-line: missing-fields
-    mauto.lsp_set_custom('gopls',{
-      filetypes = { 'go', 'templ' }
-    })
-
-  end,
+		mauto.install("templ")
+		---@diagnostic disable-next-line: missing-fields
+		mauto.lsp_set_custom("gopls", {
+			filetypes = { "go" },
+		})
+	end,
 }
