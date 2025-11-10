0a00ad7748bca258bfdc5a6819f4d4259b3dfc78
SMicucci <s.micucci00@gmail.com>
2025-11-10 17:22:49 +0100

    add(db): dbee database interface

---
+++ lua/plugins/dbee.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"kndndrj/nvim-dbee",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		build = function()
+			require("dbee").install()
+		end,
+		config = function()
+			local db = require("dbee")
+			local k = require("config.keymap")
+
+			db.setup()
+
+			k.nmap("gq", function()
+				db.toggle()
+			end, "toggle dbee UI")
+		end,
+	},
+}
