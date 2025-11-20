df27a78346d1b06f9b141b73daea74fe810827ab
Simone Micucci <simone.micucci@kbms.it>
2025-11-20 15:50:01 +0100

    fix(color): github colors work

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -6,9 +6,13 @@ return {
 	},
 	{
 		"projekt0n/github-nvim-theme",
+		name = "github_theme",
 		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme github_dark_default]])
+			require("github-theme").setup({})
+			vim.cmd("colorscheme github_dark")
+			-- vim.cmd("colorscheme one")
 			vim.o.background = "dark"
 		end,
 	},
