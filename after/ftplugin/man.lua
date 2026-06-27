2dd2f0aef65cf9141e208402321fb150fbad4348
Simone Micucci <s.micucci00@gmail.com>
2026-06-27 17:25:45 +0200

    faix(man): fix bug on keymap

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -1,8 +1,8 @@
 local k = vim.keymap
 
-k.set("n", "d", "<C-d>", { desc = "man page navigation" })
-k.set("n", "u", "<C-u>", { desc = "man page navigation" })
-k.set("n", "j", "<C-e>", { desc = "man page navigation" })
-k.set("n", "k", "<C-y>", { desc = "man page navigation" })
-k.set("n", "<C-j>", "j", { desc = "man page navigation" })
-k.set("n", "<C-k>", "k", { desc = "man page navigation" })
+k.set("n", "d", "<C-d>", { buffer = true, desc = "man page navigation" })
+k.set("n", "u", "<C-u>", { buffer = true, desc = "man page navigation" })
+k.set("n", "j", "<C-e>", { buffer = true, desc = "man page navigation" })
+k.set("n", "k", "<C-y>", { buffer = true, desc = "man page navigation" })
+k.set("n", "<C-j>", "j", { buffer = true, desc = "man page navigation" })
+k.set("n", "<C-k>", "k", { buffer = true, desc = "man page navigation" })
